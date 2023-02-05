729c7ad05292bf2d9884e14c0b5454fa43d4deb9
muzzu <>
2023-02-05 00:29:17 +0000

    initial

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template, request, redirect, url_for,Response
 from static import configFile
 from speechmatics.models import ConnectionSettings, BatchTranscriptionConfig
 from speechmatics.batch_client import BatchClient
+import os
+import soundfile
+import numpy as np
 
-def translateAudio(audio):
+def transcriptAudio(filepath):
     """implemented using speechmatics api"""
 
     LANGUAGE = "en"
-    PATH_TO_FILE = "./example.wav"
+    PATH_TO_FILE = filepath
     CONNECTION_URL = f"wss://eu2.rt.speechmatics.com/v2/{LANGUAGE}"
     AUTH_TOKEN = "89REIkjm829i85FlIVG5UFCDYmvN43qa"
 
@@ -33,7 +36,6 @@ def translateAudio(audio):
         # Note that in production, you should set up notifications instead of polling. 
         # Notifications are described here: https://docs.speechmatics.com/features-other/notifications
         transcript = client.wait_for_completion(job_id, transcription_format='txt')
-        print(transcript)
         return transcript
 
 app = Flask(__name__)
@@ -42,19 +44,25 @@ app = Flask(__name__)
 def index():
     response = requests.get("https://meowfacts.herokuapp.com/?count=1")
     data = response.json()
-    print(data)
     return render_template("index.html", data=data)
 
 @app.route("/audioToText", methods=["POST"])
 def audioToText():
     if request.method == 'POST':
-        print("-"*100, request.form['blob'])
-        audio = request.form['blob']
-        transcript = translateAudio(audio)
+        blob = request.form['blob']
+        filename = "audio.wav"
+        filepath = os.path.join("../static/audio", filename)
+        # data, samplerate = soundfile.read(blob)
+        # soundfile.write('audio.wav', data, samplerate, subtype='PCM_16')
+
+        # with open("audio.wav", 'rb') as fp:
+            # audio = fp.read()
+        # transcript = transcriptAudio(filepath)
     else:
         print("file not recieved")
     # resp = transcript
-    return "something from server"
+    transcript = str(np.random.randn(100))
+    return transcript
 
 if __name__ == '__main__':
     app.run(debug=True)
